0e92012491808d8cf3bd5766ebbe6959d66041df
antoniozandate <antonio.zandate@metricamovil.com>
2020-04-18 13:51:56 -0500

    apikey security added

--- routers/index.js
+++ routers/index.js
@@ -6,6 +6,11 @@ const ProductoRouter = require('./producto.router')
 const PurchaseRouter = require('./purchase.router')
 const SaleRouter = require('./sale.router')
 
+mainRouter.use((req, res, next) => {
+  if (req.get('API-KEY') === process.env.API_KEY) next()
+  else res.status(401).json({ error: { message: 'Invalid API Security Key', code: 401 } })
+})
+
 mainRouter.use(UsuarioRouter)
 mainRouter.use(ProductoRouter)
 mainRouter.use(PurchaseRouter)
